2062f78e9095e5e1bbf602f163900899927d4f26
DESKTOP-I7VC65A\月溪 <nodel33031@gmail.com>
2021-02-08 20:29:12 +0800

    1

---
+++ sasstest/test.js
@@ -0,0 +1,64 @@
+const form =document.getElementById('form');
+const username =document.getElementById('username');
+// const email =document.getElementById('email');
+const password =document.getElementById('password');
+const password2 =document.getElementById('password2');
+
+//Show input error message
+function showError(input, message){
+    const formControl = input.parentElement;
+    formControl.className ='form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+//Show success outline
+function showSuccess(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//Check account(email) is valid
+
+function validateEmail(username) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(username).toLowerCase());
+}
+
+
+//Event listener
+
+form.addEventListener('submit',function(e){
+    e.preventDefault();
+
+    if(username.value ===''){
+        showError(username,'請輸入帳號');
+    }
+    else if(!validateEmail(username.value)){
+        showError(username,'輸入的帳號格式錯誤');
+    
+    }
+    else{
+        showSuccess(username);
+    }
+
+    // if(email.value ===''){
+    //     showError(email,'Email is required');
+    // }else{
+    //     showSuccess(email);
+    // }
+
+    if(password.value ===''){
+        showError(password,'請輸入密碼');
+        console.log(1);
+    }else{
+        showSuccess(password);
+    }
+
+
+    if(password2.value ===''){
+        showError(password2,'請確認密碼');
+    }else{
+        showSuccess(password2);
+    }
+});
